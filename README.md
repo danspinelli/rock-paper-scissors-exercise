f496e8f2382a00539bb8cf66df157670b46cbbdb
danspinelli <84397398+danspinelli@users.noreply.github.com>
2021-05-28 14:19:51 -0400

    RPS Clean

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # rock-paper-scissors-exercise
+
+## Installation
+
+Clone or download this repo onto your local computer.
+
+Then navigate there from the command line (subsequent commands assume you are running them from the local repository's root directory):
+
+```sh
+cd my-first-python-app
+```
+
+## Setup
+
+Setup an virtual environment:
+
+```sh
+conda create -n my-rps-game-env python=3.8
+conda activate my-rps-game-env
+```
+
+Install some packages:
+
+```sh
+pip install -r requirements.txt
+```
+
+### Configuring Environment Variables
+
+TODO: also tell someone how to use environment variables. Because the .gitignore file will be ignoring our .env file it won't be included in the repo, so each new person will need to create their own local .env file. So they/you should add a new ".env" file to the root directory of this repo, and place contents like the following inside:
+
+
+```
+PLAYER_NAME="Guest 1"
+```
+
+## Usage
+
+Run the game:
+
+```sh
+python game.py
+```
--- game.py
+++ game.py
@@ -1,7 +1,20 @@
 # game.py
-import random
-print("Rock, Paper, Scissors, Shoot!")
 
+import random
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+Player_Name = os.getenv("Player_Name")
+
+
+#can separate strings with commas
+#print("Welcome", Player_Name,"! Rock, Paper, Scissors, Shoot!")
+
+#or we can concatenate with "+"
+
+print("Welcome " + Player_Name +"! Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
@@ -19,20 +32,51 @@ computer_choice = random.choice(valid_options)
 
 print("Computer choice: ", computer_choice)
 
-if (user_choice == "rock" and computer_choice == "paper"):
-    print("Computer Wins")
-elif (user_choice == "paper" and computer_choice == "rock"):
-        print("User Wins")
-elif (user_choice == "scissors" and computer_choice == "rock"):
-         print("Computer Wins")
-elif (user_choice == "scissors" and computer_choice == "paper"):
-        print("User Wins")
-elif (user_choice == "rock" and computer_choice == "scissors"):
-         print("User Wins")
-elif (user_choice == "paper" and computer_choice == "scissors"):
-        print("Computer Wins")
-else:
-    print("Tie, Play Again")
+
+#This was my way to write this code
+
+#if (user_choice == "rock" and computer_choice == "paper"):
+#   print("Computer Wins")
+#elif (user_choice == "paper" and computer_choice == "rock"):
+#       print("User Wins")
+#elif (user_choice == "scissors" and computer_choice == "rock"):
+#         print("Computer Wins")
+#elif (user_choice == "scissors" and computer_choice == "paper"):
+#        print("User Wins")
+#elif (user_choice == "rock" and computer_choice == "scissors"):
+#         print("User Wins")
+#elif (user_choice == "paper" and computer_choice == "scissors"):
+#        print("Computer Wins")
+#else:
+#    print("Tie, Play Again")
+
+
+#This is a better way to do it because it is less redundant
+
+if user_choice == computer_choice:
+    print ("Tie, Play Again")
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("IT'S A TIE")
+    elif computer_choice == "paper":
+        print("OH, THE COMPUTER WON...")
+    elif computer_choice == "scissors":
+        print("YOU WON! CONGRATS!")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("YOU WON! CONGRATS!")
+    elif computer_choice == "paper":
+        print("IT'S A TIE")
+    elif computer_choice == "scissors":
+        print("OH, THE COMPUTER WON...")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print("OH, THE COMPUTER WON...")
+    elif computer_choice == "paper":
+        print("YOU WON! CONGRATS!")
+    elif computer_choice == "scissors":
+        print("IT'S A TIE")
+
 
 
 # validate the input such that only if it is one of the expected values will we 
